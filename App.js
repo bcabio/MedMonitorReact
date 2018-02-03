16442f584d3cf2ca0635966688d8403493ee4d62
bcabio <briancabigon@gmail.com>
2018-02-03 14:39:01 -0500

    Added more components

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import { Router, Stack, Scene } from 'react-native-router-flux';
+import Tab1 from './components/Tab1/Tab1';
 
 export default class App extends Component {
   render() {
@@ -13,22 +15,27 @@ export default class App extends Component {
           <Text>Changes you make will automatically reload.</Text>
           <Text>Shake your phone to open the developer menu.</Text>
       </View>
+
         </Content>
         <Footer>
+                 
+
           <FooterTab>
-            <Button>
-              <Icon name="apps" />
-            </Button>
-            <Button>
-              <Icon name="camera" />
-            </Button>
-            <Button active>
-              <Icon active name="navigate" />
-            </Button>
-            <Button>
-              <Icon name="person" />
-            </Button>
+                <Button>
+                  <Icon name="apps" />
+                </Button>
+                <Button>
+                  <Icon name="camera" />
+                </Button>
+                <Button active>
+                  <Icon active name="navigate" />
+                </Button>
+                <Button>
+                  <Icon name="person" />
+                </Button>
           </FooterTab>
+            
+
         </Footer>
       </Container>
     );
---
+++ components/Tab1/Tab1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Tab1 extends Component {
+	render() {
+		return (
+			<div> lmao </div>
+			);		
+	}
+}
+
+export default Tab1;
