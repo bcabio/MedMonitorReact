ded09527701d1ec2025a117d14bf507d14a776ab
bcabio <briancabigon@gmail.com>
2018-02-04 05:31:53 -0500

    I tried

--- components/MedicineInputComponent/MedicineInput.js
+++ components/MedicineInputComponent/MedicineInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Stylesheet, Button, TouchableOpacity, FlatList } from 'react-native';
+import { View, Stylesheet, Button, TouchableOpacity, FlatList, ListView } from 'react-native';
 import { Container, Header, Body, Content, Form, Input, Label, Text, Item, ListItem, List, CheckBox} from 'native-base';
 
 class MedicineInput extends Component {
@@ -10,13 +10,29 @@ class MedicineInput extends Component {
 			timestamp : '',
 			meds : [], 
 			medsList : [],
-			checkBoxList: {},
+			checkBoxList: {
+				"Lasix": false,
+				"Lisinopril": false,
+				"Aricept": false,
+				"Seroquel": false,
+				"Ativan": false
+			},
 			isRendered : false
 		}
 	}
 
 
 	_keyExtractor = (item, index) => item.name;
+
+	toggleCheckBox(n) {
+		var curr = this.state;
+		console.log(curr.checkBoxList);
+
+		curr['checkBoxList'][n] = !curr['checkBoxList'][n];
+		console.log(curr.checkBoxList);
+		console.log('lmao');
+		this.setState(curr);
+	}
 
 	componentDidMount() {
 		return fetch('https://mangohacksflask.herokuapp.com/userMeds')
@@ -25,12 +41,13 @@ class MedicineInput extends Component {
 			.then((responseJSON) => {
 				console.log(responseJSON.meds);
 				var m = responseJSON.meds.map((medicine) => {
-						return <ListItem> 
+						return <ListItem onPress={() => this.toggleCheckBox(medicine.name)}> 
 										<Body>	
 											<Text> {medicine.name} </Text>
 										</Body>
 										<CheckBox 
-											checked={false}/>
+											checked={this.state.checkBoxList[medicine.name]}
+											/>
 									</ListItem>
 				})
 				console.log(m);
@@ -49,9 +66,23 @@ class MedicineInput extends Component {
 			return (
 				<Container>
 					<Content>
-						<List>
 							{this.state.medsList}
-						</List>
+
+						<FlatList
+							extraData={this.state}
+							keyExtractor={(medicine, index) => medicine.id}
+							data={this.state.medsList}
+							renderItem={(medicine) => {
+								<ListItem onPress={() => this.toggleCheckBox(medicine.name)}> 
+										<Body>	
+											<Text> {medicine.name} </Text>
+										</Body>
+										<CheckBox 
+											checked={this.state.checkBoxList[medicine.name]}
+											/>
+									</ListItem>
+							}}
+						/>
 					</Content>
 				</Container>
 
